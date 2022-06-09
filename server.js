52ed6482a55920059e7e4f4eb672f25b50eab872
Emmanuel <emmanuelomoiya6@gmail.com>
2022-06-09 09:22:12 +0000

    chills

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
   const bodyParser = require('body-parser');
